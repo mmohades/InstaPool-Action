1151852fd97eb237bda98af6b3974fd72aeb0819
mmohades <mmohades@hotmail.com>
2019-11-02 15:07:42 -0400

    Init project

---
+++ src/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const { App } = require('jovo-framework');
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+const app = new App();
+
+app.use(
+    new Alexa(),
+    new GoogleAssistant(),
+    new JovoDebugger(),
+    new FileDb()
+);
+
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+    LAUNCH() {
+        return this.toIntent('testIntent');
+    },
+
+    testIntent() {
+        return this.tell("Test works");
+    },
+
+    MyNameIsIntent() {
+        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+    },
+});
+
+module.exports.app = app;
